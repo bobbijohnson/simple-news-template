aa493ebc01b8017daea770df8776dcaf98f1abd9
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-22 14:41:05 +0000

    updated for fitness class template

---
+++ folderstructure.sh
@@ -0,0 +1,59 @@
+mkdir articles
+mkdir webpage-templates
+mkdir articles/nutrition
+mkdir articles/workouts
+mkdir articles/sports
+mkdir articles/technology
+mkdir articles/fashion
+mkdir articles/travel
+mkdir articles/mental-health
+mkdir articles/lifestyle
+cp section-home.html articles/nutrition/section-home.html
+cp article.html articles/nutrition/article1.html
+cp article.html articles/nutrition/article2.html
+cp article.html articles/nutrition/article2.html
+cp article.html articles/nutrition/article4.html
+cp article.html articles/nutrition/article5.html
+cp section-home.html articles/workouts/section-home.html
+cp article.html articles/workouts/article1.html
+cp article.html articles/workouts/article2.html
+cp article.html articles/workouts/article2.html
+cp article.html articles/workouts/article4.html
+cp article.html articles/workouts/article5.html
+cp section-home.html articles/sports/section-home.html
+cp article.html articles/sports/article1.html
+cp article.html articles/sports/article2.html
+cp article.html articles/sports/article2.html
+cp article.html articles/sports/article4.html
+cp article.html articles/sports/article5.html
+cp section-home.html articles/technology/section-home.html
+cp article.html articles/technology/article1.html
+cp article.html articles/technology/article2.html
+cp article.html articles/technology/article2.html
+cp article.html articles/technology/article4.html
+cp article.html articles/technology/article5.html
+cp section-home.html articles/fashion/section-home.html
+cp article.html articles/fashion/article1.html
+cp article.html articles/fashion/article2.html
+cp article.html articles/fashion/article2.html
+cp article.html articles/fashion/article4.html
+cp article.html articles/fashion/article5.html
+cp section-home.html articles/travel/section-home.html
+cp article.html articles/travel/article1.html
+cp article.html articles/travel/article2.html
+cp article.html articles/travel/article2.html
+cp article.html articles/travel/article4.html
+cp article.html articles/travel/article5.html
+cp section-home.html articles/mental-health/section-home.html
+cp article.html articles/mental-health/article1.html
+cp article.html articles/mental-health/article2.html
+cp article.html articles/mental-health/article2.html
+cp article.html articles/mental-health/article4.html
+cp article.html articles/mental-health/article5.html
+cp section-home.html articles/lifestyle/section-home.html
+cp article.html articles/lifestyle/article1.html
+cp article.html articles/lifestyle/article2.html
+cp article.html articles/lifestyle/article2.html
+cp article.html articles/lifestyle/article4.html
+cp article.html articles/lifestyle/article5.html
+
