01f5122e529142764f70573ad5e6fe8cdfe3bd5c
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-24 12:40:04 +0000

    folders again

--- image-folders.sh
+++ image-folders.sh
@@ -7,11 +7,11 @@
 # mkdir articles/mental-health/images
 # mkdir articles/lifestyle/images
 
-touch articles/nutrition/images README.txt
-touch articles/workouts/images README.txt
-touch articles/sports/images README.txt
-touch articles/technology/images README.txt
-touch articles/fashion/images README.txt
-touch articles/travel/images README.txt
-touch articles/mental-health/images README.txt
-touch articles/lifestyle/images README.txt
+touch articles/nutrition/images/README.txt
+touch articles/workouts/images/README.txt
+touch articles/sports/images/README.txt
+touch articles/technology/images/README.txt
+touch articles/fashion/images/README.txt
+touch articles/travel/images/README.txt
+touch articles/mental-health/images/README.txt
+touch articles/lifestyle/images/README.txt
